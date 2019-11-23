a222f72d66184da5c9e367b65f93a9f45f43e505
zhanghay <2867334718@qq.com>
2019-11-23 22:14:48 +0800

    logic src

--- 4-Logic/logic.py
+++ 4-Logic/logic.py
@@ -15,16 +15,16 @@ def sigmoid(inX):
     return 1.0/(1+exp(-inX))
 
 def gradAscent(dataMatIn, classLabels):
-    dataMatrix = mat(dataMatIn)             #convert to NumPy matrix
-    labelMat = mat(classLabels).transpose() #convert to NumPy matrix
+    dataMatrix = mat(dataMatIn)             #矩阵转换
+    labelMat = mat(classLabels).transpose() #矩阵转换
     m,n = shape(dataMatrix)
     alpha = 0.001
     maxCycles = 500
     weights = ones((n,1))
-    for k in range(maxCycles):              #heavy on matrix operations
-        h = sigmoid(dataMatrix*weights)     #matrix mult
-        error = (labelMat - h)              #vector subtraction
-        weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
+    for k in range(maxCycles):              #
+        h = sigmoid(dataMatrix*weights)     #
+        error = (labelMat - h)              #
+        weights = weights + alpha * dataMatrix.transpose()* error #
     return weights
 
 def plotBestFit(weights):
@@ -52,7 +52,7 @@ def plotBestFit(weights):
 def stocGradAscent0(dataMatrix, classLabels):
     m,n = shape(dataMatrix)
     alpha = 0.01
-    weights = ones(n)   #initialize to all ones
+    weights = ones(n)   #
     for i in range(m):
         h = sigmoid(sum(dataMatrix[i]*weights))
         error = classLabels[i] - h
@@ -61,12 +61,12 @@ def stocGradAscent0(dataMatrix, classLabels):
 
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = shape(dataMatrix)
-    weights = ones(n)   #initialize to all ones
+    weights = ones(n)   #全1
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
-            alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
-            randIndex = int(random.uniform(0,len(dataIndex)))#go to 0 because of the constant
+            alpha = 4/(1.0+j+i)+0.0001    # 
+            randIndex = int(random.uniform(0,len(dataIndex)))#
             h = sigmoid(sum(dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]

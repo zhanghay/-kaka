2d068a8b3cd0acdf4192de168c93d12ac0e9bc63
zhanghay <2867334718@qq.com>
2019-12-07 20:45:14 +0800

    svm example

--- 2-Tree/treePlotter.py
+++ 2-Tree/treePlotter.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt 
 
-decisionNode=dict(boxstyle="sawtooth",fc="0.8")
+decisionNode=dict(boxstyle="sawtooth",fc="0.8")#形状
 leafNode=dict(boxstyle="round4",fc="0.8")
 arrow_args=dict(arrowstyle="<-")
 
@@ -12,6 +12,6 @@ def createPlot():
     fig=plt.figure(1,facecolor='white')
     fig.clf()
     createPlot.ax1=plt.subplot(111,frameon=False)
-    plotNode(U'决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
+    plotNode(U'决策节点',(0.5,0.1),(0.1,0.5),decisionNode)#名称，末端，起始端
     plotNode(U'；叶节点',(0.8,0.1),(0.3,0.8),leafNode)
     plt.show()
